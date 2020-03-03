1c902cf9ebd6ec0126aa53eaec53a79e4e2c6ca8
laxmanbro <ee18btech11049@iith.ac.in>
2020-03-03 22:56:35 +0530

    presentation

--- presentation 1/fig_code.py
+++ presentation 1/fig_code.py
@@ -1,18 +1,18 @@
 from scipy import signal
 import matplotlib.pyplot as plt
 s1 = signal.TransferFunction([1], [1, 3, 2])  # TF = 1/(s^2+3s+2)
-s2 = signal.TransferFunction([1], [1, 4, 6, 4])  # TF = 1/(s^3+4s^2+6s+4)
+s2 = signal.TransferFunction([1], [1, 3, 3])  # TF = 1/(s^3+4s^2+6s+4)
 
 
-w, mag, phase = signal.bode(s1)
-h, mag, phase = signal.bode(s2)
+w, mag1, phase1 = signal.bode(s1)
+h, mag2, phase2 = signal.bode(s2)
 
 plt.figure()
 plt.title('magnitude plot')
 plt.xlabel('frequency rad/s')
 plt.ylabel('magnitude dB')
-plt.semilogx(w, mag)
-plt.semilogx(h, mag)
+plt.semilogx(w, mag1)
+plt.semilogx(h, mag2)
 
 # plt.grid()
 
